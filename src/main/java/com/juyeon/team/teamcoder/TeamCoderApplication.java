de2faffc13ee7bdd05375ff5a0737f0bd571129d
Juyeon-Lee <ljo0831@naver.com>
2020-11-05 12:44:56 +0900

    Initial commit

---
+++ src/main/java/com/juyeon/team/teamcoder/TeamCoderApplication.java
@@ -0,0 +1,13 @@
+package com.juyeon.team.teamcoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamCoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamCoderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juyeon/team/teamcoder/TeamCoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juyeon.team.teamcoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamCoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
